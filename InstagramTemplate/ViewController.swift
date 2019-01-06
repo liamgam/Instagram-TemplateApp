a77e1818e1e2d2c563580383bcdb0034f53340dd
liamgam <liam.liamgamble@gmail.com>
2019-01-05 18:40:21 -0800

    bottom and top of profile page

---
+++ InstagramTemplate/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  InstagramTemplate
+//
+//  Created by Rinni Swift on 1/5/19.
+//  Copyright © 2019 Rinni Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(displayP3Red: 250/255, green: 250/255, blue: 250/255, alpha: 1)
+        
+    }
+
+
+}
+
