eb7a22b926bb05b8f8eb6653f64135e2f615405d
liamgam <liam.liamgamble@gmail.com>
2019-01-06 22:33:06 -0800

    keyboard dismiss, UI look of app set

--- InstagramTemplate/ViewController.swift
+++ InstagramTemplate/ViewController.swift
@@ -9,14 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: labels
+    @IBOutlet weak var usernameTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // set instagram color background
         view.backgroundColor = UIColor(displayP3Red: 250/255, green: 250/255, blue: 250/255, alpha: 1)
         
     }
-
-
+    
+    // use to make the view or any subview that is the first responder resign (optionally force)
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
