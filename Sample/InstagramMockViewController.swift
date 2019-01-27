79ab9487236c41cd3367cac10603c789ef18e477
liamgam <liam.liamgamble@gmail.com>
2019-01-27 10:42:38 -0800

    added constraints to the profile labels and profile image

--- Sample/InstagramMockViewController.swift
+++ Sample/InstagramMockViewController.swift
@@ -10,14 +10,10 @@ import UIKit
 
 class InstagramMockViewController: UIViewController {
 
-    @IBOutlet weak var profileImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        profileImage.clipsToBounds = true
-        profileImage.layer.cornerRadius = 50
-        profileImage.backgroundColor = UIColor(red: 0.86, green: 0.86, blue: 0.86, alpha: 1)
         
     }
 }
